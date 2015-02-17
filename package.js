966838c30da8ff4566ad9f65a2e1d03b2a4ca883
Tim Heckel <tim@slatebox.com>
2015-02-17 15:19:39 -0600

    Upgrading for Meteor 1.0

--- package.js
+++ package.js
@@ -1,11 +1,13 @@
 Package.describe({
     summary: "Bootstrap switch icons (iOS style)"
+    , name: "mrt:bootstrap-switch"
+    , version: "1.9.0"
 });
 
 Package.on_use(function (api) {
-    api.use('jquery', 'client');
+    api.use('jquery@1.0.1', 'client');
     api.add_files([
         'lib/jquery.switch.js'
-	,'css/bootstrap-switch.css'
+		,'css/bootstrap-switch.css'
     ], 'client');
 });
